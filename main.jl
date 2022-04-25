4d64d22b07c3d59a64b508014125550a0610c66d
Elias Sörstrand <elisor1219@hotmail.com>
2022-04-25 23:54:47 +0200

    Added plot functionality to the code

--- main.jl
+++ main.jl
@@ -1,4 +1,6 @@
-using JuMP, AxisArrays ,Gurobi, UnPack, CSV, DataFrames, PlotlyJS
+using JuMP, AxisArrays ,Gurobi, UnPack, CSV, DataFrames, PlotlyJS, Format
+
+pathToFigures = "figures/exercise1"
 
 #TODO: LookOver the KW and KWH and how we use it. I think something may be off.
 
@@ -9,6 +11,8 @@ include("input_energisystemprojekt.jl")
         PV_cf, wind_cf = read_input()
 
 m = Model(Gurobi.Optimizer)
+set_optimizer_attribute(m, "NumericFocus", 3)
+set_optimizer_attribute(m, "BarHomogeneous", 1)
 
 function annualisedCost(investmentCost, years)
     investmentCost * ((discountrate)/(1-(1/((1+discountrate)^years))))
@@ -16,7 +20,6 @@ end
 
 #Constans that will be used in the model
 RESERVOIR_MAX_SIZE = 33*1000000                             #[MWh]
-# NOT IN USE [(TWh*1 000 000) / h] = [MW]
 
 println("\nSetting variables...")
 @variables m begin
@@ -130,14 +133,49 @@ else
     error("The model was not solved correctly.")
 end
 
-systemCost = objective_value(m)/1000000 # M€
+#Find the systemcost for diffrent regions
+regionCost = AxisArray(zeros(length(REGION)), REGION)
+for r in REGION
+    regionCost[r] = value(sum(RunnigCost[r,p] for p in PLANT)) +
+    value(sum(AnnualisedInvestment[r,p] for p in PLANT)) +
+    value(sum(FuelCost[r,p] for p in PLANT))
+end
+
+systemCost = objective_value(m) # €
 Capacity_result = value.(Capacity)
 
-emissionResult = value(sum(Emission)) #Ton CO_2
+Emission = Emission*1000 # Ton CO_2 to CO_2 
+totalEmissionResult = value(sum(Emission)) # CO_2
 
 
-println("Total cost: ", systemCost, " M€")
-println("Total emissions: ", emissionResult, " Ton(k) CO_2")
+
+
+#This part is for formating and printing the value of the cost and emission v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v
+#Formating the values
+formatedValuesCost = AxisArray(Vector{Union{Nothing, String}}(nothing, length(REGION)), REGION)
+formatedValuesCost[:DE] = format(regionCost[:DE], precision=0, commas=true )
+formatedValuesCost[:SE] = format(regionCost[:SE], precision=0, commas=true )
+formatedValuesCost[:DK] = format(regionCost[:DK], precision=0, commas=true )
+
+formatedValuesCO_2 = AxisArray(Vector{Union{Nothing, String}}(nothing, length(REGION)), REGION)
+formatedValuesCO_2[:DE] = format(value(Emission[:DE, :Gas]), precision=0, commas=true )
+formatedValuesCO_2[:SE] = format(value(Emission[:SE, :Gas]), precision=0, commas=true )
+formatedValuesCO_2[:DK] = format(value(Emission[:DK, :Gas]), precision=0, commas=true )
+
+#Printing the values
+println("Total cost: ", format(systemCost, precision=0, commas=true ), " €")
+println("\tGermany: ", formatedValuesCost[:DE], " €")
+println("\tSweden: ", formatedValuesCost[:SE], " €")
+println("\tDenmark: ", formatedValuesCost[:DK], " €")
+println("\n")
+println("Total emissions: ", format(totalEmissionResult, precision=0, commas=true ), " CO_2")
+println("\tGermany: ", formatedValuesCO_2[:DE], " CO_2")
+println("\tSweden: ", formatedValuesCO_2[:SE], " CO_2")
+println("\tDenmark: ", formatedValuesCO_2[:DK], " CO_2")
+#This part is for formating and printing the value of the cost and emission ^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
+
+
+
 
 #Calculating the total power generated from diffrent PLANTS in diffrent REGIONS
 Power = AxisArray(zeros(length(REGION), length(PLANT)), REGION, PLANT)
@@ -153,7 +191,9 @@ for h in HOUR, p in PLANT, r in REGION
 end
 
 
-#Here begins the plotting part------------------------------
+
+
+#Here begins the plotting part v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v
 time = 24*2
 newAvrageTime = 1:floor(Int,HOUR[end]/time)
 AverageDayPower = AxisArray(zeros(floor(Int,length(HOUR)/time), length(PLANT), length(REGION)), 1:HOUR[end]/time, PLANT, REGION)
@@ -166,70 +206,136 @@ for d in newAvrageTime, p in PLANT, r in REGION
     AverageDayPower[d,p,r] = average
 end
 
-#timeInterval = 147:147+23
+
 timeInterval = 147:651
-#timeInterval = HOUR
 
 
-#Ploting the average domestic generation of Sweden 
-SE_df = DataFrame(Hour=newAvrageTime, 
+
+#Ploting the average domestic generation of Germany v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v
+df = DataFrame(TowDays=newAvrageTime, 
+                  Wind=AverageDayPower[newAvrageTime,:Wind,:DE],
+                  Solar=AverageDayPower[newAvrageTime,:PV,:DE],
+                  Gas=AverageDayPower[newAvrageTime,:Gas,:DE],
+                  Hydro=AverageDayPower[newAvrageTime,:Hydro,:DE],
+)
+
+long_df = stack(df, Not([:TowDays]), variable_name="Production", value_name="MW")
+
+p1 = plot(long_df,
+    kind="bar",
+    x=:TowDays,
+    y=:MW,
+    color=:Production,
+    Layout(title="The twoday-average energy production in Germany",
+    barmode="stack",
+    bargap=0)
+    )
+
+
+
+
+#Ploting the average domestic generation of Sweden v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v
+df = DataFrame(TowDays=newAvrageTime, 
                   Wind=AverageDayPower[newAvrageTime,:Wind,:SE],
                   Solar=AverageDayPower[newAvrageTime,:PV,:SE],
                   Gas=AverageDayPower[newAvrageTime,:Gas,:SE],
                   Hydro=AverageDayPower[newAvrageTime,:Hydro,:SE],
 )
 
-long_SE_df = stack(SE_df, Not([:Hour]), variable_name="Production", value_name="MW")
+long_df = stack(df, Not([:TowDays]), variable_name="Production", value_name="MW")
 
-plot(long_SE_df,
+p2 = plot(long_df,
     kind="bar",
-    x=:Hour,
+    x=:TowDays,
     y=:MW,
     color=:Production,
-    Layout(title="The twoday-average energy production Sweden",
+    Layout(title="The twoday-average energy production in Sweden",
     barmode="stack",
     bargap=0)
- )
+    )
 
 
-#Ploting the domestic generation of Sweden 
-SE_df = DataFrame(Hour=timeInterval, 
-                  Wind=HourPower[timeInterval,:Wind,:SE],
-                  Solar=HourPower[timeInterval,:PV,:SE],
-                  Gas=HourPower[timeInterval,:Gas,:SE],
-                  #Nuclear=HourPower[timeInterval,:Nuclear,:SE]
-                  Hydro=HourPower[timeInterval,:Hydro,:SE],
+
+
+#Ploting the average domestic generation of Denmark v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v
+df = DataFrame(TowDays=newAvrageTime, 
+                  Wind=AverageDayPower[newAvrageTime,:Wind,:DK],
+                  Solar=AverageDayPower[newAvrageTime,:PV,:DK],
+                  Gas=AverageDayPower[newAvrageTime,:Gas,:DK],
+                  Hydro=AverageDayPower[newAvrageTime,:Hydro,:DK],
 )
 
-long_SE_df = stack(SE_df, Not([:Hour]), variable_name="Production", value_name="MW")
+long_df = stack(df, Not([:TowDays]), variable_name="Production", value_name="MW")
 
-plot(long_SE_df,
+p3 = plot(long_df,
     kind="bar",
-    x=:Hour,
+    x=:TowDays,
     y=:MW,
     color=:Production,
-    Layout(title="Energy production Sweden",
+    Layout(title="The twoday-average energy production in Denmark",
     barmode="stack",
     bargap=0)
- )
+    )
+
+ 
 
 
-#Ploting the domestic generation of Germany 
-DE_df = DataFrame(Hour=timeInterval,
+
+
+#Ploting the domestic generation of Germany v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v
+df = DataFrame(Hour=timeInterval,
                   Wind=HourPower[timeInterval,:Wind,:DE],
                   Solar=HourPower[timeInterval,:PV,:DE],
                   Gas=HourPower[timeInterval,:Gas,:DE],
                   #Nuclear=HourPower[timeInterval,:Nuclear,:DE]
 )
 
-long_DK_df = stack(DE_df, Not([:Hour]), variable_name="Production", value_name="MW")
+long_df = stack(df, Not([:Hour]), variable_name="Production", value_name="MW")
 
-plot(long_DK_df,
+p4 = plot(long_df,
     kind="bar",
     x=:Hour,
     y=:MW,
     color=:Production,
-    Layout(title="Energy production in Germany",
+    Layout(title="Energy production in Germany between hour 147 and 651.",
     barmode="stack",
     bargap=0)
  )
+
+
+
+
+
+
+#Ploting the diffrent plants in use v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v
+region = ["Germany", "Sweden", "Denmark"]
+
+p5 = plot(
+    [
+        bar(name="Wind", x=region, y=Power[:,:Wind]),
+        bar(name="Solar", x=region, y=Power[:,:PV]),
+        bar(name="Gas", x=region, y=Power[:,:Gas]),
+        bar(name="Hydro", x=region, y=Power[:,:Hydro]),
+    ],
+    Layout(
+        title="Energy production in diffrent regions and plants.",
+        yaxis_title="MW"
+    )
+)
+relayout!(p, barmode="group")
+
+display(p1)
+display(p2)
+display(p3)
+display(p4)
+display(p5)
+
+savefig(p1, string(pathToFigures,"/germany.svg"))
+savefig(p2, string(pathToFigures,"/sweden.svg"))
+savefig(p3, string(pathToFigures,"/denmark.svg"))
+savefig(p4, string(pathToFigures,"/germany147-651.svg"))
+savefig(p5, string(pathToFigures,"/plants.svg"))
+
+
+
+
